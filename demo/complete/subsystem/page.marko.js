55a288c7665a2202800ff03ae42e085c99c00c80
Matteo Collina <hello@matteocollina.com>
2018-11-12 17:35:29 +0100

    Updated demo

--- demo/complete/subsystem/page.marko.js
+++ demo/complete/subsystem/page.marko.js
@@ -1,7 +1,11 @@
-// Compiled using marko@4.4.28 - DO NOT EDIT
+// Compiled using marko@4.13.8 - DO NOT EDIT
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/fastify-demo$1.0.0/subsystem/page.marko",
+    components_helpers = require("marko/src/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
     marko_helpers = require("marko/src/runtime/html/helpers"),
     marko_loadTag = marko_helpers.t,
     component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
@@ -9,27 +13,33 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
     init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
 
-function render(input, out) {
+function render(input, out, __component, component, state) {
   var data = input;
 
   out.w("<!DOCTYPE html><html lang=\"en\"><head></head><body>");
 
   component_globals_tag({}, out);
 
-  out.w("<h1>Hello " +
+  out.w("<h1>Ciao " +
     marko_escapeXml(input.hello) +
     "</h1>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out);
+  await_reorderer_tag({}, out, __component, "4");
 
   out.w("</body></html>");
 }
 
-marko_template._ = render;
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
+    id: "/fastify-demo$1.0.0/subsystem/page.marko",
     tags: [
       "marko/src/components/taglib/component-globals-tag",
       "marko/src/components/taglib/init-components-tag",
